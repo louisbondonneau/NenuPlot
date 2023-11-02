a1e799df4f56be0c84813b9e89a0092518e609a3
bondonneau louis <louis.bondonneau@me.com>
2023-11-02 13:48:33 +0100

    clean all master

--- uploadto.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def uptodatabf2(path_from, path_to):
-    cmd="""rsync -av -e \"ssh \"  %s nfrplsobs@databfnfrdt:/data/nenufar-pulsar/%s""" % (path_from, path_to)
-    p=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-    output, errors = p.communicate()
-    print(errors,output)
