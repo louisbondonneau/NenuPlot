ef73439d19d36978bf901c39b33b8fbac44958df
bondonneau louis <louis.bondonneau@me.com>
2024-03-12 15:21:37 +0100

    4.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NenuPlot',
-    version='4.0',
+    version='4.0.1',
     python_requires='>=2.7',
     description='NenuPlot is a tool designed to assist in the visualization and analysis of PSRFITS folded files. It provides a quick-look generation in PDF and PNG formats and offers numerous options for data handling, including cleaning, rebinding, RM and DM fitting, and data extraction within specified frequency and time ranges.',
     author='Louis Bondonneau',
