f026c21b1522a0ed8918b77b6eb9477a8cc0cec8
bondonneau louis <louis.bondonneau@me.com>
2024-06-28 10:06:24 +0200

    check key befor pop

--- NenuPlot_module/RM_fit_class.py
+++ NenuPlot_module/RM_fit_class.py
@@ -21,9 +21,11 @@ CONFIG_FILE = os.path.dirname(os.path.realpath(__file__)) + '/' + 'NenuPlot.conf
 class RM_fit_class(DM_fit_class):
     def __init__(self, *args, **kwargs):
         self.rm_window = kwargs.get('rm_window', None)
-        kwargs.pop('rm_window')
+        if "rm_window" in kwargs:
+            kwargs.pop('rm_window')
         self.plot_QU = kwargs.get('plot_QU', False)
-        kwargs.pop('plot_QU')
+        if "plot_QU" in kwargs:
+            kwargs.pop('plot_QU')
         super(RM_fit_class, self).__init__(*args, **kwargs)
         rmdelt_60mhz = 0.002
         self.rmdelt = (rmdelt_60mhz / 60**2) * (self.centre_frequency)**2
